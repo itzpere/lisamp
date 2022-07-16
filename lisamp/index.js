68fc6332d0e2544ec37707125398416d00ce1642
itzpere <pere@itzpere.com>
2022-07-16 21:37:06 +0200

    works

--- lisamp/index.js
+++ lisamp/index.js
@@ -1,55 +1,40 @@
-
-// importing discord and its relevant classes
-const { Client, Intents, Discord } = require('discord.js');
-const token = "OTk2ODM2OTg2OTQ4MTU3NDYw.GYte0H.ddc1kYyNi8v29-Lnhq3_eKpKfxnM4If6Nryxqg";
-const client = new Discord.Client({ 
-    intents : intents.ALL
-
- });
-
-const PREFIX = "!";
-
-const fs = require('fs');
-
-client.commands = new Discord.Collection();
-
-// Implementing command folder
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles)
-{
-    const command = require(`./commands/${file}`);
-    
-    client.commands.set(command.name, command);
-}
-
-// listening for event to notify progress
-client.on('ready', () => {
-    console.log("Connected as " + client.user.tag);
-    })
-
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, token, guildid } = require('./config.json');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 //commands
-client.on('interactionCreate', message => {
-    if(!message.content.startsWith(PREFIX) || message.author.bot) return;
-    const args = message.content.substring(PREFIX.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-    switch (command){
-        case 'hello':
-            client.command.get('hello').execute(message, args);
-        break;
-        case 'clear':
-            client.command.get('clear').execute(message, args);
-        break;
-        case 'play':
-            client.command.get('play').execute(message, args);
-        break;
-        case 'leave':
-            client.command.get('leave').execute(message, args);
-        break;
-    }
-})
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
 
-//super sercet token
+const rest = new REST({ version: '9' }).setToken(token);
+//register /
+rest.put(Routes.applicationGuildCommands(clientId, guildid), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+//ready
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+//commands
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
 client.login(token);
-
-
 // https://www.youtube.com/watch?v=fN29HIaoHLU
