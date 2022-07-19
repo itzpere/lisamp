c54ad3e23baed21b71104ab1e09d61f420f3d602
itzpere <pere@itzpere.com>
2022-07-19 20:01:42 +0200

    intent and !server

--- commands/Default/server.js
+++ commands/Default/server.js
@@ -1,13 +1,12 @@
 const { guildId } = require('../../config.json');
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const guild = client.guilds.cache.get(guildId);
-
+const client = new Client({ intents: 3243773 });
+//const guild = client.guilds.cache.get(guildId);
 module.exports = {
     callback: (message, ...args) => {
 
         console.log("Server name: ",args);
-        message.reply(`Server name: ${guild.name}\nTotal members: ${guild.memberCount}`);
-        
+        console.log(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+        message.reply(`**Server name**: ${message.guild.name}\n**Total members**: ${message.guild.memberCount}\n**Created at**: ${message.guild.createdAt}\n`);
     },
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { token, prefix } = require('./config.json');
 const { Client, Intents, Collection } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: 3243773 });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
