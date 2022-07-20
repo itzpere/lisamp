4ab217ca1b0708e011ff4276487ffb1bcc4ad64a
itzpere <pere@itzpere.com>
2022-07-20 16:00:30 +0200

    help+

--- commands/Default/help.js
+++ commands/Default/help.js
@@ -3,10 +3,23 @@ module.exports = {
     callback: (message, ...args) => {
         console.log("help: ",args);
         var txt = "";
+        var arg = args.shift();
+        if (arg !== undefined)
+        {
+            console.log("arg is not null: ", arg);
+            for(var index in data) { 
+                if (index == arg){
+                console.log(index, " is ", arg);
+                message.reply(`**Name**: ${arg}\n**Description**: ${data[arg]?.Description}`);
+                }
+            }
+        }
+        else {
         for(var index in data) { 
             txt+= index + ", ";
         }
         message.reply(`**Available Commands**: ${txt.slice(0, -2)}`);
+    }
     },
 }
 //TODO filter za komande, da se filteruje da li je komanda aktivna i da li je ispravna
