2b50c0e2dfc9076824b7e7c00df1d304ff2f1ca0
itzpere <pere@itzpere.com>
2022-07-21 23:35:49 +0200

    touch ups 1

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const { token, prefix } = require('./config.json');
-const { Client, Intents, Collection } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: 3243773 });
-const { Player } = require("discord-player");
-require("discord-player/smoothVolume");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -15,15 +13,15 @@ client.on('ready', () => {
 
 	handler(client);
   });
-  
+
 client.login(token);
 console.log("Token: OK")
 
 /* config.json:
 
 {
-	"clientId": "YourClientID",
 	"token"   : "YourToken",
+	"clientId": "YourClientID",
 	"guildId" : "YourGuildId",
   	"prefix"  : "!",
 	"suffix"  : ".js"
