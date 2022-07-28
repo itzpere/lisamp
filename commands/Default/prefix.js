a5eb3944809f597ce0afa7163a04a48b305e2b07
itzpere <pere@itzpere.com>
2022-07-28 19:16:57 +0200

    prefix added ""

--- commands/Default/prefix.js
+++ commands/Default/prefix.js
@@ -3,12 +3,12 @@ module.exports = {
         console.log("prefix: ",args);
         const { getServerData, setServerData } = require("../../ServerData.js")
         let prefix = getServerData(message, "prefix")
-        if (args == "") {message.channel.send(`Current prefix is: **${prefix}**`)}
+        if (args == "") {message.channel.send(`Current prefix is: "**${prefix}**"`)}
         else {
             const newprefix = args.shift()
             const file = getServerData(message, "file")
             setServerData(message,"prefix", newprefix)
-            message.channel.send(`Prefix changed to: ${newprefix}`);
+            message.channel.send(`Prefix changed to: "**${newprefix}**"`);
         }
     }
 }
