1f50fc0dccbddac1393a6c2875b92599920bf89e
itzpere <pere@itzpere.com>
2022-07-29 16:08:25 +0200

    config command added

--- embeds.js
+++ embeds.js
@@ -1,6 +1,7 @@
-const { MessageEmbed } = require('discord.js');
-function playing (queue, track){
+const { MessageEmbed, Guild } = require('discord.js');
+const getServerData = require("./ServerData").getServerData
 
+function playing (queue, track){
 	const playing = new MessageEmbed()
 		.setColor(0x0099FF)
 		.setTitle(track.title)
@@ -15,4 +16,23 @@ function playing (queue, track){
 		.setFooter({ text: track.id, iconURL: 'https://cdn.discordapp.com/app-icons/996836986948157460/0d45bfa4728b32e3b8f3e4c71da8fa84.png?size=256' });
 	queue.metadata.channel.send({embeds: [playing]})
 }
-module.exports.currentlyplaying = playing;
+function config (message){
+	let guild = message.guild
+	let jsonFile = require(getServerData(message,"file"))
+	let jsonString = JSON.stringify(jsonFile,null,4)
+	const config = new MessageEmbed()
+		.setColor(0x0099FF)
+		.setTitle(`Config for ${guild.name}`)
+		.setThumbnail(guild.iconURL)
+		.addFields(
+			{ name: 'Config', value: jsonString, inline: true }
+		)
+		.setTimestamp()
+		.setFooter({text: guild.id, iconURL: 'https://cdn.discordapp.com/app-icons/996836986948157460/0d45bfa4728b32e3b8f3e4c71da8fa84.png?size=256' });
+	message.channel.send({embeds:[config]})
+}
+module.exports = {
+	currentlyplaying : playing,
+	serverConfig : config
+
+}
