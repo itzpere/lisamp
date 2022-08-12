520186e61e4924715b325419772958777b1e81bc
Petar Miletic <107474038+itzpere@users.noreply.github.com>
2022-08-12 23:25:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,46 +17,48 @@ fi
 #Advanced Setup
 echo -e "\nDo you want to go to advanced mode?\nThe default values will be writen for everything if NO"
 read -p "YES / NO : " advancedMode
+echo read
 if [ -z "$advancedMode" ]; then
-echo -e "\nIncorrect input, it needs to be exacly the same"
-echo "Setup Failed"
-exit 0
-if [ $advancedMode != "YES" -a $advancedMode != "NO" ]; then
-echo "Incorrect input, it needs to be exacly the same"
-echo "Setup Failed"
-exit 0
-elif [ $advancedMode == "YES" ]
-#prefix
-then
-echo "  Press ENTER if you want default option"
-echo -e "\nDefault prefix* | default is '!' :"
-read prefix
-if [ -z "$prefix" ]; then
-prefix="!"
+    echo -e "\nIncorrect input, it needs to be exacly the same"
+    echo "Setup Failed"
+    exit 0
 fi
+if [ $advancedMode != "YES" -a $advancedMode != "NO" ]; then
+    echo "Incorrect input, it needs to be exacly the same"
+    echo "Setup Failed"
+    exit 0
+elif [ $advancedMode == "YES" ]
+    #prefix
+    then
+    echo "  Press ENTER if you want default option"
+    echo -e "\nDefault prefix* | default is '!' :"
+    read prefix
+    if [ -z "$prefix" ]; then
+    prefix="!"
+    fi
 #musicrole
-echo -e "\nDefault musicRole* | default is '' :"
-read musicrole
+    echo -e "\nDefault musicRole* | default is '' :"
+    read musicrole
 #repeat
-echo -e "\nDefault repeat* | default is 'off'\n   Options: OFF, QUEUE, TRACK, AUTOPLAY :"
-read repeat
+    echo -e "\nDefault repeat* | default is 'off'\n   Options: OFF, QUEUE, TRACK, AUTOPLAY :"
+    read repeat
 if [ -z "$repeat" ]; then
-repeat=0
+    repeat=0
 elif [ repeat == "QUEUE" ]; then
-repeat=1
+    repeat=1
 elif [ repeat == "TRACK" ]; then
-repeat=2
+    repeat=2
 elif [ repeat == "AUTOPLAY" ]; then
-repeat=3
+    repeat=3
 else
-repeat=0
+    repeat=0
 fi
 #yt
-echo -e "\nYoutube cookie* | default is \"\" :"
-read ytcookie
+    echo -e "\nYoutube cookie* | default is \"\" :"
+    read ytcookie
 #spotify
-echo -e "\nSpotify cookie | default is \"\" :"
-read spcookie
+    echo -e "\nSpotify cookie | default is \"\" :"
+    read spcookie
 #write file
 echo "{ \"token\" : \"$token\",\"prefix\" : \"$prefix\",\"musicrole\" : \"$musicrole\",\"repeat\" : \"$repeat\",\"ytcookie\" : \"$ytcookie\",\"spcookie\" : \"$spcookie\"}" > config.json
 echo "Setup Complete"
@@ -64,5 +66,4 @@ exit 0
 else
 echo "{ \"token\" : \"$token\",\"prefix\" : \"!\",\"musicrole\" : \"\",\"repeat\" : \"0\",\"ytcookie\" : \"\",\"spcookie\" : \"\"}" > config.json
 echo "Setup Complete"
-fi
 fi
